05765e481b0e8bc953801050bdc396f9d2b96f85
ghbhaha <ghbhaha@gmail.com>
2019-11-07 19:06:00 +0100

    Settings: Add three-fingers-swipe to screenshot [2/3]
    This is a squash of the following commits:
    
      Author: ghbhaha <ghbhaha@gmail.com>
      Date: 2019-11-07 19:06:00 +0100
    
          Settings: Add three-fingers-swipe to screenshot [2/2]
    
          * Thanks Sahul Krishan for the beautiful animation
    
          Signed-off-by: ghbhaha <ghbhaha@gmail.com>
          Change-Id: I8d5671870830a561311aa4c6f2d0d2fb14e382d6
          Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    
      Author: Mesquita <mesquita@aospa.co>
      Date: 2021-10-20 20:12:43 -0300
    
          Settings: Update Swipe to Screenshot preference
    
          * Thanks to Andrew Fluck for the animation
    
          Signed-off-by: Mesquita <mesquita@aospa.co>
          Change-Id: I4d3cd44c65f4b8262e9b521c87f7df1ed831a750
    
      Author: Akash Srivastava <akashniki@gmail.com>
      Date: 2021-10-09 20:13:29 +0000
    
          Settings: Use new material-you illustration for 3-finger screenshot
    
          * Switch to new Android 12 animation. Huge thanks to @Aryanata for the animation
    
          Co-authored-by: Aryanata Andipradana <a.ryan.ata.ap@gmail.com>
          Change-Id: I71e4d51a59756dff846b42dee35dabc052c6e590
    
    Change-Id: I8d5671870830a561311aa4c6f2d0d2fb14e382d6
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- src/com/android/settings/gestures/GesturesSettingPreferenceController.java
+++ src/com/android/settings/gestures/GesturesSettingPreferenceController.java
@@ -22,6 +22,7 @@ import android.hardware.display.AmbientDisplayConfiguration;
 import androidx.annotation.NonNull;
 
 import co.aospa.settings.gestures.PowerButtonTorchGesturePreferenceController;
+import co.aospa.settings.gestures.SwipeToScreenshotPreferenceController;
 import co.aospa.settings.gestures.VolumeButtonMusicControlPreferenceController;
 
 import com.android.settings.core.BasePreferenceController;
@@ -73,6 +74,7 @@ public class GesturesSettingPreferenceController extends BasePreferenceControlle
         controllers.add(new PreventRingingParentPreferenceController(context, FAKE_PREF_KEY));
         controllers.add(new PowerButtonTorchGesturePreferenceController(context, FAKE_PREF_KEY));
         controllers.add(new VolumeButtonMusicControlPreferenceController(context, FAKE_PREF_KEY));
+        controllers.add(new SwipeToScreenshotPreferenceController(context, FAKE_PREF_KEY));
         return controllers;
     }
 }
